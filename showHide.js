20112d84ec35971ea5e83e7801967a5af4f8c56a
RobertoNK <roberto.dg2611@gmail.com>
2018-09-26 13:58:41 -0400

    committing html and javacript file

---
+++ showHide.js
@@ -0,0 +1,8 @@
+function flipImage(){
+    dom = document.getElementById('saturn').style;
+    if(dom.visibility == "visible"){
+        dom.visibility="hidden";
+    }else{
+        dom.visibility="visible";
+    }
+}
